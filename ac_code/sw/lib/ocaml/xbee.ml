d7d4a6ea50b61b5bf733dc217853fd247520d289
lijie <lij@efficien.cn>
2017-05-25 15:25:13 +0800

    lift err bug

--- ac_code/sw/lib/ocaml/xbee.ml
+++ ac_code/sw/lib/ocaml/xbee.ml
@@ -151,10 +151,10 @@ let api_tx64 = fun ?(frame_id = 0) dest data ->
     s.[3] <- Char.chr 0x13;
     s.[4] <- Char.chr 0xa2;
     s.[5] <- Char.chr 0x00;
-    s.[6] <- Char.chr 0x41;
-    s.[7] <- Char.chr 0x4F;
-    s.[8] <- Char.chr 0x0C;
-    s.[9] <- Char.chr 0xBF; (*no6*)
+    s.[6] <- Char.chr 0x40;
+    s.[7] <- Char.chr 0xFC;
+    s.[8] <- Char.chr 0xE1;
+    s.[9] <- Char.chr 0x29; (*no6*)
 	
 	  (*s.[2] <- Char.chr 0x00;
     s.[3] <- Char.chr 0x13;
