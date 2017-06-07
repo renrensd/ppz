6a3bb6758f001d6f4ef608ceb4178432b1e6daab
lijie <lij@efficien.cn>
2017-06-07 15:42:15 +0800

    new test board2
    Conflicts:
    	ac_code/sw/ground_segment/tmtc/link.ml
    	ac_code/sw/lib/ocaml/xbee.ml

--- ac_code/sw/lib/ocaml/xbee.ml
+++ ac_code/sw/lib/ocaml/xbee.ml
@@ -165,14 +165,14 @@ let api_tx64 = fun ?(frame_id = 0) dest data ->
     s.[8] <- Char.chr 0x1F;
     s.[9] <- Char.chr 0x90;*) (*no10*)
     
-    s.[2] <- Char.chr 0x00;
+    (*s.[2] <- Char.chr 0x00;
     s.[3] <- Char.chr 0x13;
     s.[4] <- Char.chr 0xA2;
     s.[5] <- Char.chr 0x00;
     s.[6] <- Char.chr 0x41;
     s.[7] <- Char.chr 0x4F;
     s.[8] <- Char.chr 0x0D;
-    s.[9] <- Char.chr 0x54; (*no10*)
+    s.[9] <- Char.chr 0x54;*) (*no10*)
 	
 	  (*s.[2] <- Char.chr 0x00;
     s.[3] <- Char.chr 0x13;
@@ -244,7 +244,16 @@ let api_tx64 = fun ?(frame_id = 0) dest data ->
     s.[6] <- Char.chr 0x40;
     s.[7] <- Char.chr 0xFB;
     s.[8] <- Char.chr 0xA0;
-    s.[9] <- Char.chr 0x80;*) (*debugpc*)
+    s.[9] <- Char.chr 0x80;*) (*test board*)
+    
+    s.[2] <- Char.chr 0x00;
+    s.[3] <- Char.chr 0x13;
+    s.[4] <- Char.chr 0xA2;
+    s.[5] <- Char.chr 0x00;
+    s.[6] <- Char.chr 0x41;
+    s.[7] <- Char.chr 0x4E;
+    s.[8] <- Char.chr 0x7E;
+    s.[9] <- Char.chr 0x91; (*test board*)
 
    (* s.[2] <- Char.chr 0x00;
     s.[3] <- Char.chr 0x13;
