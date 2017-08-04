2b8c1483804cea2657aa1569951fb8e36ae99ca0
lg <lig@efficient.cn>
2017-08-04 16:43:11 +0800

    fix bug

--- ac_code/sw/airborne/subsystems/mission/task_process.c
+++ ac_code/sw/airborne/subsystems/mission/task_process.c
@@ -682,7 +682,7 @@ bool_t run_normal_task(void)
 			if(stateGetHorizontalSpeedNorm_f() < 0.3) /*make sure hover motion setted*/
 			{
 				hover_flag = FALSE;
-				VECT2_COPY(home_wp_enu, wp_home);
+				VECT2_COPY(home_wp_enu, vertipad_land);
 
 #ifdef USE_PLANED_OA
 				planed_oa.wp_move_done_flag = TRUE;
